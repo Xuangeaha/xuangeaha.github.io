a28007edba7df027ecdc91dda800af014dae7ac6
轩哥啊哈OvO <108124782+Xuangeaha@users.noreply.github.com>
2023-04-29 18:34:02 +0800

    2.1.0

---
+++ src/header.js
@@ -0,0 +1,3 @@
+"use strict";
+var header = "<div id=\"footer\">\n    <div id=\"footer-text\">\n        <div class=\"header-text\">\n        <img src=\"../../favicon.png\" width=30>&nbsp;\n        \u8F69\u54E5\u554A\u54C8OvO\u7684\u5B98\u65B9\u7F51\u7AD9<a href=\"https://xuangeaha.github.io/\" class=\"back\"><<< \u8FD4\u56DE\u4E3B\u9875</a></div>\n        </div>\n    </div>\n</div>";
+document.writeln(header);
