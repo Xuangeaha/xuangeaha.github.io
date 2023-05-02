cc48de16242e0224856aa2772e481e28633da4ee
轩哥啊哈OvO <108124782+Xuangeaha@users.noreply.github.com>
2023-05-02 22:52:25 +0800

    3.1.2

--- src/header.ts
+++ src/header.ts
@@ -1,11 +1,12 @@
 const header: string = `<div id="headerbar">
     <div id="footer-text">
-        <img src="../../favicon.png" width=30>&nbsp;
-        轩哥啊哈OvO的官方网站
-        <a href="https://xuangeaha.github.io/" class="header-text">主页</a>
-        <a href="https://xuangeaha.github.io/doc/" class="header-text">文档</a>
-        <a href="https://xuangeaha.github.io/tools/" class="header-text">工具</a>
-        <a href="https://xuangeaha.github.io/chatgpt/" class="header-text">ChatGPT镜像</a>
+        <img src="../../favicon.png" width=30 alt="轩哥啊哈OvO">&nbsp;
+        轩哥啊哈OvO的官方网站&emsp;
+        <a href="https://xuangeaha.github.io/" class="header-text">主页</a>&emsp;
+        <a href="https://xuangeaha.github.io/doc/" class="header-text">文档</a>&emsp;
+        <a href="https://xuangeaha.github.io/tools/" class="header-text">工具</a>&emsp;
+        <a href="https://xuangeaha.github.io/chatgpt/" class="header-text">ChatGPT镜像</a>&emsp;
+        <a href="https://xuangeaha.github.io/about/" class="header-text">关于</a>
     </div>
 </div>`;
 
