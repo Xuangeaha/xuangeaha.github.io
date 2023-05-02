3d908c048fbe2a0cf3425ea3131b4cf5cdc1e20c
轩哥啊哈OvO <108124782+Xuangeaha@users.noreply.github.com>
2023-05-02 18:19:53 +0800

    3.1.0

--- src/header.ts
+++ src/header.ts
@@ -1,9 +1,11 @@
 const header: string = `<div id="headerbar">
     <div id="footer-text">
         <img src="../../favicon.png" width=30>&nbsp;
-        轩哥啊哈OvO的官方网站&emsp;
-        <a href="https://xuangeaha.github.io/chatgpt/" class="header-text">ChatGPT</a>
-        <a href="https://xuangeaha.github.io/" class="back"><<< 返回主页</a></div>
+        轩哥啊哈OvO的官方网站
+        <a href="https://xuangeaha.github.io/" class="header-text">主页</a>
+        <a href="https://xuangeaha.github.io/doc/" class="header-text">文档</a>
+        <a href="https://xuangeaha.github.io/tools/" class="header-text">工具</a>
+        <a href="https://xuangeaha.github.io/chatgpt/" class="header-text">ChatGPT镜像</a>
     </div>
 </div>`;
 
